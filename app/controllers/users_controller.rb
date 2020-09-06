9d3036c5d29c97826db776b798733484245e1d7c
joannaylin <lin.joanna.y@gmail.com>
2020-09-05 21:26:21 -0500

    added index route for users for root

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def index
+    users = User.all
+    render json: users
+  end
+
   def create
     user = User.find_by(username: params[:username])
     if !user
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "users#create"
-  
-  resources :users, only: [:create]
+  resources :users, only: [:index, :create]
   get "/users/:username", to: "users#show"
-
+  
   resources :movies, only: [:create, :destroy]
+
+  root "users#index"
 end
